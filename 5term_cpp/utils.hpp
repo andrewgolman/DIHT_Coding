ab200d3acf667cbd4940d6f580a3eafd6888c2df
andrewsg <andrewsgolman@gmail.com>
2018-11-23 03:15:38 +0300

    use tuple instead of vector in equation return

--- 5term_cpp/utils.hpp
+++ 5term_cpp/utils.hpp
@@ -2,6 +2,9 @@
 
 constexpr double UT_EPS = 0.0000001;
 
+template <typename T>
+using QERoots = std::tuple<size_t, T, T>;
+
 template <typename Type>
 int compare_to_zero(Type val) {
     if (val > UT_EPS) {
